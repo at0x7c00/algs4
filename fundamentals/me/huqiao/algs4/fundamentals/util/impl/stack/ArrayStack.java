19952572f003cd9bbdeb2780e561b3fe0eea6b91
xooxle <zoozle@qq.com>
2017-02-10 09:31:30 +0800

    add ConcurrentModificationException when modify stack at stack is iterating.

--- fundamentals/me/huqiao/algs4/fundamentals/util/impl/stack/ArrayStack.java
+++ fundamentals/me/huqiao/algs4/fundamentals/util/impl/stack/ArrayStack.java
@@ -1,5 +1,6 @@
 package me.huqiao.algs4.fundamentals.util.impl.stack;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
 import me.huqiao.algs4.fundamentals.util.Stack;
@@ -14,12 +15,15 @@ public class ArrayStack<Item> implements Stack<Item> {
 	private Item[] items = (Item[])new Object[20];
 	private int size = 0;
 	
+	private long modifyCounter = 0;
+	
 	@Override
 	public void push(Item item) {
 		if( items.length <= size ){
 			resize(items.length*2);
 		}
 		items[size++] = item;
+		modifyCounter++;
 	}
 	
 	private void resize(int s) { 
@@ -38,6 +42,7 @@ public class ArrayStack<Item> implements Stack<Item> {
 		if(size<items.length/4){
 			resize(items.length/2);
 		}
+		modifyCounter++;
 		return item;
 	}
 
@@ -58,13 +63,28 @@ public class ArrayStack<Item> implements Stack<Item> {
 	
 	class ArrayQueueInterator implements Iterator<Item>{
 
+		private long modifyCounter;
+		public ArrayQueueInterator(){
+			modifyCounter = ArrayStack.this.modifyCounter;
+		}
+		
 		@Override
 		public boolean hasNext() {
+			if(modifyed()){
+				throw new ConcurrentModificationException();
+			}
 			return !isEmpty();
+		}
+
+		private boolean modifyed() {
+			return modifyCounter!=ArrayStack.this.modifyCounter;
 		}
 
 		@Override
 		public Item next() {
+			if(modifyed()){
+				throw new ConcurrentModificationException();
+			}
 			return pop();
 		}
 
@@ -82,6 +102,7 @@ public class ArrayStack<Item> implements Stack<Item> {
 		
 		for(Integer s : stack){
 			System.out.println(s);
+			stack.push(2000);
 		}
 	}
 }
